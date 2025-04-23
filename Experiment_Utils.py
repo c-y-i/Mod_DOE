8f1d8f05232f61b7074577fd037c83bd1277dce9
gcampbell <gcampbel@seas.upenn.edu>
2025-04-23 10:24:10 -0400

    Updated experiment run into .ipynb - should be intuitive to run now.

--- Experiment_Utils.py
+++ Experiment_Utils.py
@@ -9,22 +9,24 @@
 # Current data from an Adafruit INA260 breakout board via ESP32 Pico D4
 
 import os
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-if os.name == 'nt':
-    import msvcrt
-    def getch():
-        return msvcrt.getch().decode()
-else:
-    import sys, tty, termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    def getch():
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+# if os.name == 'nt':
+#     import msvcrt
+#     def getch():
+#         return msvcrt.getch().decode()
+# else:
+#     import sys, tty, termios
+#     fd = sys.stdin.fileno()
+#     # old_settings = termios.tcgetattr(fd)
+#     def getch():
+#         try:
+#             tty.setraw(sys.stdin.fileno())
+#             ch = sys.stdin.read(1)
+#         finally:
+#             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#         return ch
 
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
 from Serial_Comms import *
@@ -40,11 +42,15 @@ from queue import Queue                   # Import queue
 
 # ESP32 Pico D4 features
 esp_serial = '/dev/ttyUSB0' # CHECK - may be 0 or 1
-esp_baud = 115200
+# esp_baud = 115200
+esp_baud = 500_000 #ESP default baudrate : 115200
 
 # Dictionary details
 dict_file = 'v2_test_dict.pkl'
 in_num = 1
+
+# file variable for recording
+rec_bool = True
 
 #############################################################################
 # Dynamixel Features
@@ -67,7 +73,7 @@ PROTOCOL_VERSION            = 2.0               # See which protocol version is 
 
 # Default setting
 DXL_ID                      = 35                # Dynamixel ID : 1
-BAUDRATE                    = 3000000           # Dynamixel default baudrate : 57600
+BAUDRATE                    = 3_000_000           # Dynamixel default baudrate : 57600
 DEVICENAME                  = '/dev/ttyUSB1'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
@@ -78,7 +84,7 @@ DXL_MINIMUM_POSITION_VALUE  = 10           # Dynamixel will rotate between this 
 DXL_MAXIMUM_POSITION_VALUE  = 4000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
 DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
 
-index = 0
+direction = 0
 dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
 SPEED = [100, 1124]
 STOP = 0
@@ -172,133 +178,225 @@ def dxl_read_address(ADDR, portHandler = portHandler, packetHandler = packetHand
         print("Position ERROR %s" % packetHandler.getRxPacketError(dxl_error))
     return dxl_address_value
 
-# Read current from ESP32 serial
-def read_current(q, serial_port, baud_rate=115200):
-    current_vals = []
-    timestamp = []
-    init_time = time.time()
-    while(RECORD_BOOL):
-        in_current = -1
-        while in_current == -1:
-            with serial.Serial(port=esp_serial, baudrate=esp_baud, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) as ser:
-                line = ser.readline().decode('utf-8').rstrip()
-                if line:
-                    value = parse_string(line, float_only=True)[0]
-                    if value is not None:
-                        in_current = value
-        current_vals.append(in_current)
-        timestamp.append(int((time.time()-init_time)*1000))
-    print(f'Posting current vals, shape: {len(current_vals)}')
-    q.put( [timestamp, current_vals] )
-
+# read in the 'percent torque' value from the dynamixel, return to queue 'q'
 def dxl_read_loads(q):
     load_vals = []
     timestamp = []
     init_time = time.time()
-    while(RECORD_BOOL):
+    while(rec_bool):
         load = int(dxl_read_address(ADDR_MX_PRESENT_LOAD))
         load_vals.append(load)
         timestamp.append(int((time.time()-init_time)*1000))
     print(f'Posting load vals, shape: {len(load_vals)}')
     q.put( [timestamp, load_vals] )
 
+# Read current from ESP32 serial, return to queue 'q'
+def read_current(q, serial_port, baud_rate=esp_baud):
+    raw = []
+    timestamp = []
+    init_time = time.time()
+    loop_count = 0
+    with serial.Serial(port=esp_serial, baudrate=esp_baud, bytesize=8) as ser:
+        while(rec_bool):
+            if ser.in_waiting > 0:
+                line = ser.readline()
+                # if line:
+                raw.append(line)
+                timestamp.append(int((time.time()-init_time)*1000))
+            loop_count += 1
+
+    current_vals = []
+    for row in raw:
+        line = row.decode('utf-8').rstrip()
+        try:
+            value = parse_string(line, float_only=True)[1]
+            current_vals.append(value)
+        except:
+            value = parse_string(line, float_only=True)
+            if len(value):
+                value = value[0]
+                try:
+                    current_vals.append(value)
+                except:
+                    current_vals.append(-1)
+            else:
+                current_vals.append(-1)
+        
+
+    print(f'Posting current vals, shape: {len(current_vals)}')
+    q.put( [timestamp, current_vals, np.ones(len(timestamp))] ) # third column to differentiate currents
+
+
+##############################################
+# Plotting Function
+##############################################
+def plot_dict_key(in_dict, dict_key):
+    fig = plt.figure()
+    # 2 subplots
+    ax = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+
+    n = len(in_dict[dict_key])
+
+    # set up colors
+    base_c1 = cm.Reds
+    base_c2 = cm.Blues
+    color1 = [base_c1(i / n) for i in range(n)]
+    color2 = [base_c2(i / n) for i in range(n)]
+
+    # plot all the trials
+    dxt_all_vals = []
+    current_all_vals = []
+    for i in range(n):
+        current_time = np.array(in_dict[dict_key][i][1][0])
+        current_vals = np.array(in_dict[dict_key][i][1][1])
+        dxt_time = np.array(in_dict[dict_key][i][0][0])
+        dxt_vals = np.array(in_dict[dict_key][i][0][1])
+
+        if np.mean(dxt_vals) > 1024: # CW
+            dxt_percent = (dxt_vals - 1024) / 1023
+        else:
+            dxt_percent =  - dxt_vals / 1023 #CCW
+        ax.scatter(dxt_time, dxt_percent, c=color1[i], label='torque')
+        ax2.scatter(current_time, current_vals, c=color2[i], label='current')
+        dxt_all_vals.extend(dxt_percent)  # Collect all values
+        current_all_vals.extend(current_vals)
+
+    ax.set_xlabel('time (ms)')
+    ax.set_ylabel('dxt torque (%)')
+    ax2.set_xlabel('time (ms)')
+    ax2.set_ylabel('current (mA)')
+    current_ll = np.percentile(current_all_vals, 1)
+    current_ul = np.percentile(current_all_vals, 99)
+    dxt_ll = np.percentile(dxt_all_vals, 1)
+    dxt_ul = np.percentile(dxt_all_vals, 99)
+    ax.set_ylim([dxt_ll, dxt_ul])
+    ax2.set_ylim([current_ll, current_ul])
+
+    plt.tight_layout()
+    plt.show()
+
 #################################################
 # Main Loop
 #################################################
 
-# Set control type
-# Open port
-if portHandler.openPort():
-    print("Succeeded to open the port")
-else:
-    print("Failed to open the port")
-    print("Press any key to terminate...")
-    getch()
-    quit()
-
-
-# Set port baudrate
-if portHandler.setBaudRate(BAUDRATE):
-    print("Succeeded to change the baudrate")
-else:
-    print("Failed to change the baudrate")
-    print("Press any key to terminate...")
-    getch()
-    quit()
-
-dxl_set_Control('wheel')
-dxl_set_Torque_enable(True)
-
-# create an empty array
-dxl_inputs = np.zeros([0,2]).astype(int)
-esp_inputs = np.zeros([0,1]).astype(int)
-
-while(1):
-    print("Press any key to continue! (or press ESC to quit!)")
-    if getch() == chr(0x1b):
-        break
+def Run_Experiment(in_dict, RUNTIME = 2, in_key = 'test', DIR = direction, file_dir = None, file_name = None, save = True):
+    # DIR bool sets direction
+    if DIR:
+        dir_str = 'CCW'
+    else:
+        dir_str = 'CW'
     
+    # Set control type
+    # Open port
+    if portHandler.openPort():
+        print("Succeeded to open the port")
+    else:
+        print("Failed to open the port")
+        # print("Press any key to terminate...")
+        # getch()
+        quit()
+
+
+    # Set port baudrate
+    if portHandler.setBaudRate(BAUDRATE):
+        print("Succeeded to change the baudrate")
+    else:
+        print("Failed to change the baudrate")
+        # print("Press any key to terminate...")
+        # getch()
+        quit()
+
+    # Use continuous motion mode
+    dxl_set_Control('wheel')
+    dxl_set_Torque_enable(True)
+
+    # create an empty array
+    dxl_inputs = np.zeros([0,2]).astype(int)
+    esp_inputs = np.zeros([0,1]).astype(int)
+
+    # while(1):
+    #     print("Press any key to continue! (or press ESC to quit!)")
+    #     if getch() == chr(0x1b):
+    #         break
+        
+    print(f'Running motor {dir_str} for {RUNTIME} seconds ...')
+
     # use a Queue for thread-safe communication
     output_queue = Queue()
 
     thread_current = threading.Thread(target=read_current, args=(output_queue, esp_serial, esp_baud))
     thread_dxl = threading.Thread(target=dxl_read_loads, args=(output_queue,))
 
-    RECORD_BOOL = True
+    global rec_bool 
+    rec_bool = True
     # Start threads
     thread_current.start()
 
     # # Write goal speed
     dxl_set_LED(True)
-    dxl_move_speed(SPEED[index])
+    dxl_move_speed(SPEED[DIR])
     start_time = time.time()
 
     thread_dxl.start()
 
     # Wait for run
-    while(time.time()-start_time < 2):
+    while(time.time()-start_time < RUNTIME):
         pass
 
-    RECORD_BOOL = False
+    rec_bool = False
     thread_dxl.join()
-    dxl_inputs = output_queue.get()
+    read_queue1 = output_queue.get()
 
     dxl_set_LED(False)
     dxl_move_speed(STOP)
 
-    # turn off RECORD_BOOL and collect results
+    # turn off rec_bool and collect results
     thread_current.join()
-    esp_inputs = output_queue.get()
+    read_queue2 = output_queue.get()
+
+    # make sure esp and dxl values are sorted correctly
+    if len(read_queue2) == 2:
+        dxl_inputs = read_queue2
+        esp_inputs = read_queue1[:-1]
+    else:
+        dxl_inputs = read_queue1
+        esp_inputs = read_queue2[:-1]
 
     # Change goal position
-    if index == 0:
-        index = 1
+    if DIR == 0:
+        direction = 1
     else:
-        index = 0
+        direction = 0
 
-dxl_set_Torque_enable(False)
+    dxl_set_Torque_enable(False)
 
-portHandler.closePort()
+    portHandler.closePort()
 
-all_inputs = [dxl_inputs,esp_inputs]
+    all_inputs = [dxl_inputs,esp_inputs]
 
-# add to existing dictionary
-key_name = 'test' + str(in_num)
+    # add to existing dictionary
+    key_name = in_key
 
-os.chdir('Mod_DOE')
-# dict_set = {}
-dict_set = pickle.load(open(dict_file, "rb"))
-# see if key exists
-if key_name in dict_set.keys():
-    print("Key exists, appending")
-    dict_set[key_name].append(all_inputs)
-else:
-    dict_set[key_name] = [all_inputs] # save as a list
-    print("Key does not exist, creating new key")
-pickle.dump(dict_set, open(dict_file, "wb"))
+    dict_set = in_dict
 
+    # see if key exists
+    if key_name in dict_set.keys():
+        print("Key exists, appending")
+        dict_set[key_name].append(all_inputs)
+    else:
+        dict_set[key_name] = [all_inputs] # save as a list
+        print("Key does not exist, creating new key")
 
-# shape : [[nx1,nx1],nx1,nx1] - n is number of samples
-# shape : [mxn,mxn,mxn,mxn ... ] - m is number of TRIALS (... is number of reported values)
-    # concatenate over m trials - > [nx1,nx1,nx1,nx1] (samples)
-        # report single value: [1,0] - cost (i.e. max(current))
+    if save and file_dir and file_name:
+        init_dir = os.getcwd()
+        os.chdir(file_dir)
+        pickle.dump(dict_set, open(file_name, "wb"))
+        os.chdir(init_dir)
+
+    return dict_set
+
+    # shape : [[nx1,nx1],nx1,nx1] - n is number of samples
+    # shape : [mxn,mxn,mxn,mxn ... ] - m is number of TRIALS (... is number of reported values)
+        # concatenate over m trials - > [nx1,nx1,nx1,nx1] (samples)
+            # report single value: [1,0] - cost (i.e. max(current))
